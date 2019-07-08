d6bffd37019c4089bf452b0c5de5562f7905a5ef
TakeruTakeru <vitual.money.mo.money.tkr@gmail.com>
2019-07-08 20:29:17 +0900

    add build folder

---
+++ build/precache-manifest.81d1bc48d6dcdb193fca2abebb7a9ca9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a104620a2533d1bb236424054b7e9b26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e3661a7154eaf26c52f",
+    "url": "/static/css/2.6c1f7cf1.chunk.css"
+  },
+  {
+    "revision": "544c2a4b775dcc264157",
+    "url": "/static/css/main.0df7f008.chunk.css"
+  },
+  {
+    "revision": "4e3661a7154eaf26c52f",
+    "url": "/static/js/2.07d672fd.chunk.js"
+  },
+  {
+    "revision": "544c2a4b775dcc264157",
+    "url": "/static/js/main.ac819311.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
